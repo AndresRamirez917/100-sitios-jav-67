f2cf4a98260d155134fdcc1d2bdb0c7d01504ea9
AndresRamirez917 <andresmintic2021@gmail.com>
2024-11-27 19:10:46 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 async function getData(){
-    const result = await fetch('https://rickandmortyapi.com/api/character');
+    const result = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
     const character = await result.json();
     console.log(character)
     console.log(character)
-
+    character.drinks.forEach(element => {
+        const img = document.createRange().createContextualFragment(`
+            
+            <h2>lok</h2>
+            <img src="${element.strDrinkThumb}" alt="">
+            
+            
+            `)
+            const content = document.getElementById('showcase');
+            content.append(img)
+    });
 }
 getData()
 
